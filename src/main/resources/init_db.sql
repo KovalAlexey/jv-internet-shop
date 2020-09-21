87b111a282b776c6dccb02837d6d4c8e3bc007ad
Alexey <gradiviro@gmail.com>
2020-09-21 11:38:21 +0300

    init SQL update to current DB

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -4,5 +4,5 @@ CREATE TABLE `internet-shop`.`products` (
   `product_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
   `productName` VARCHAR(225) NOT NULL,
   `price` DOUBLE NOT NULL,
-  `deleted` TINYINT NULL,
+  `deleted` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`product_id`));
